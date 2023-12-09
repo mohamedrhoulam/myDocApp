0ba1b5296896f19bc30adb6997333d2c569c991a
rhxmoha <m.rhoulam@aui.ma>
2023-12-09 20:28:47 +0100

    added appointments routes

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mydocdb",
-  password: "mohaMED01",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = {
