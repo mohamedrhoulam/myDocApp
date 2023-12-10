feb50d689e37e1a77463f8de905fa73c46603e6c
rhxmoha <m.rhoulam@aui.ma>
2023-12-10 13:40:41 +0100

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import Overview from "./Components/Overview/Overview";
 
 function App() {
   return (
+    <Router>
       <Sidebar />
+    </Router>
   );
 }
 
